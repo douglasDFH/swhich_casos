8e31a827fa00443e213ef5ee95211e8d1d0a0a9b
douglasDFH <155786486+douglasDFH@users.noreply.github.com>
2024-01-16 16:24:45 -0400

    Add files via upload
    ejercicios en clase buscar la función para convertir a mayusculas las vocales

---
+++ switch_case.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include<cctype>
+using namespace std;
+
+int main(int argc, char *argv[]) {
+char caracter;
+cout<<"ingrese una letra :"; cin>> caracter;
+//buscar la funcionpara convertir a mayusculas
+caracter = tolower(caracter);
+switch(caracter)
+{
+case 'a': 
+case 'e':
+case 'i': 
+case 'o': 
+case 'u': cout<< "es una vocal";
+	break;
+default: cout<<"no es una vocal";
+};
+	
+	return 0;
+}
+
